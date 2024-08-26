404620b21bdfce9bae854e50c96a6ef5fad5ba5d
Anushree <idekaranushree@gmail.com>
2024-08-26 16:51:49 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    
+    themeToggleBtn.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+    });
+});
